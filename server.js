897d812cb556b82fb561b92aa96e2d6785ef7286
Naim Boufridi <boufridi.naim@gmail.com>
2023-08-03 14:04:27 -0400

    API key check for Hyperbeam

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const express = require('express')
 const axios = require('axios')
 const app = express()
 
+// API Key Check
+const apiKey = process.env.HB_API_KEY
+
+if (!apiKey || apiKey === "") {
+    console.error("API Key is not set, did you set the HB_API_KEY environment variable to your API key?")
+}
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'))
 })
@@ -15,8 +22,9 @@ app.get('/computer', async (req, res) => {
     return
   }
   const resp = await axios.post('https://engine.hyperbeam.com/v0/vm', {}, {
-    headers: { 'Authorization': `Bearer ${process.env.HB_API_KEY}` }
+    headers: { 'Authorization': `Bearer ${apiKey}` }
   })
+
   computer = resp.data
   res.send(computer)
 })
